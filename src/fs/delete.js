8095ec0e64601b8c1c828ad19be7bb09c8d83253
Vitali <gimvit@gmail.com>
2024-10-03 19:08:47 +0200

    feat: implement remove

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,24 @@
+import { access, rm } from "node:fs/promises";
+import path from "node:path";
+
+const folderPath = "src/fs/files";
+const fileToRemove = "fileToRemove.txt";
+
+const checkPath = async (destPath) => {
+  try {
+    await access(path.resolve(destPath));
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
 const remove = async () => {
-    // Write your code here 
+  const pathToRemove = path.join(folderPath, fileToRemove);
+  const isFileExist = await checkPath(pathToRemove);
+  if (!isFileExist) throw new Error("FS operation failed");
+
+  await rm(pathToRemove);
 };
 
 await remove();
