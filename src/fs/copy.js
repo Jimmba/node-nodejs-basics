948a6a566ca4d03a613f34c882bad00b2c1b1b77
Vitali <gimvit@gmail.com>
2024-10-03 18:51:16 +0200

    feat: implement copy logic

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,38 @@
+import { access, copyFile, mkdir, readdir } from "node:fs/promises";
+import path from "node:path";
+
+const copyFrom = "src/fs/files";
+const copyTo = "src/fs/files_copy";
+
+const checkPath = async (destPath) => {
+  try {
+    await access(path.resolve(destPath));
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+const copyDirectory = async () => {
+  const files = await readdir(copyFrom);
+  for (let file of files) {
+    const fromPath = path.join(copyFrom, file);
+    const toPath = path.join(copyTo, file);
+
+    await copyFile(fromPath, toPath);
+  }
+};
+
 const copy = async () => {
-    // Write your code here 
+  const isCopyFromExist = await checkPath(copyFrom);
+  if (!isCopyFromExist) throw new Error("FS operation failed");
+
+  const isCopyToExist = await checkPath(copyTo);
+  if (isCopyToExist) throw new Error("FS operation failed");
+
+  await mkdir(copyTo);
+
+  await copyDirectory();
 };
 
 await copy();
