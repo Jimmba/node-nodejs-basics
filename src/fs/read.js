ece7da5332d9d5ee1b9fe05f07a194faf1692f3a
Vitali <gimvit@gmail.com>
2024-10-03 19:17:54 +0200

    feat: implement read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,25 @@
+import { access, readFile } from "node:fs/promises";
+import path from "node:path";
+
+const folderPath = "src/fs/files";
+const fileToRead = "fileToRead.txt";
+
+const checkPath = async (destPath) => {
+  try {
+    await access(path.resolve(destPath));
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(folderPath, fileToRead);
+  const isFileExist = await checkPath(filePath);
+  if (!isFileExist) throw new Error("FS operation failed");
+
+  const data = await readFile(filePath, "utf8");
+  console.log(data);
 };
 
 await read();
