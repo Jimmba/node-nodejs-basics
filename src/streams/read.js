3c34a861beedd41d16a1d014edb29b22db58a028
Vitali <gimvit@gmail.com>
2024-10-05 23:34:35 +0200

    feat: implement read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,11 @@
+import { createReadStream } from "fs";
+import { stdout } from "process";
+
+const filePath = "src/streams/files/fileToRead.txt";
+
 const read = async () => {
-    // Write your code here 
+  const stream = createReadStream(filePath);
+  stream.pipe(stdout);
 };
 
 await read();
